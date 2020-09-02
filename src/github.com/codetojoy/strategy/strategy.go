bd7657db2f07e1ba7398d8e53f5b7b4d19624130
Michael Easter <codetojoy@gmail.com>
2020-09-02 17:40:36 -0300

    nearest card

--- src/github.com/codetojoy/strategy/strategy.go
+++ src/github.com/codetojoy/strategy/strategy.go
@@ -10,12 +10,15 @@ type Strategy interface {
 }
 
 func BuildStrategy(which string) Strategy {
-    result := nextCard{}
+    var result Strategy
 
+    const NEAREST_CARD = "NearestCard"
     const NEXT_CARD = "NextCard"
 
     if strings.EqualFold(NEXT_CARD, which) {
-       // no-op
+        result = nextCard{}
+    } else if strings.EqualFold(NEAREST_CARD, which) {
+        result = nearestCard{}
     }
 
     return result
@@ -26,4 +29,33 @@ type nextCard struct {
 
 func (nc nextCard) SelectCard(prizeCard int, cards []int, maxCard int) int {
     return cards[0]
+}
+
+type nearestCard struct {
+}
+
+func absValue(x int) int {
+    result := x
+
+    if x < 0 {
+        result = -1 * x
+    }
+
+    return result
+}
+
+func (nc nearestCard) SelectCard(prizeCard int, cards []int, maxCard int) int {
+    result := 0
+    bestAbsValue := maxCard * 10
+
+    for _, card := range cards {
+        thisAbsValue := absValue(card - prizeCard)
+
+        if thisAbsValue < bestAbsValue {
+            bestAbsValue = thisAbsValue
+            result = card
+        }
+    }
+
+    return result
 }
--- src/github.com/codetojoy/strategy/strategy_test.go
+++ src/github.com/codetojoy/strategy/strategy_test.go
@@ -22,3 +22,21 @@ func TestNextCard(t *testing.T) {
         t.Errorf("NextCard actual: %d expected: %d", result, expected)
     }
 }
+
+func TestNearestCard(t *testing.T) {
+    const prizeCard = 10
+    cards := []int{4,6,9,12}
+    const maxCard = 12
+    strategy := BuildStrategy("nearestcard")
+
+    // test
+    result := strategy.SelectCard(prizeCard, cards, maxCard)
+
+    // TODO: figure out how to use test-assert library (see README.md)
+    expected := 9
+    ok := (result == expected)
+
+    if ! ok {
+        t.Errorf("NearestCard actual: %d expected: %d", result, expected)
+    }
+}
