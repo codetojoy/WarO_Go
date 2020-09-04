e9fb7167b92a8b5850520aae1076f8ba32020a77
Michael Easter <codetojoy@gmail.com>
2020-09-04 05:19:16 -0300

    incremental update Fri  4 Sep 2020 05:19:16 ADT

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+CONFIG_FILE=$1
+if [ ! -f "$CONFIG_FILE" ]; then
+    echo "could not find config file: '$CONFIG_FILE'"
+    exit -1
+fi
+
 export GOPATH=$PWD
 
-go run src/github.com/codetojoy/runner.go config.json 
+go run src/github.com/codetojoy/runner.go $CONFIG_FILE 
